a6ae59f3a690f50f5cec961b786676266a39d2d1
felipeliins <lins_felipe@outlook.com>
2024-03-23 18:49:22 -0300

    inicial

---
+++ Inicial/todo-project/src/pages/home/index.js
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+
+function Home(){
+    return(
+        <Container>
+            <h1>SEJA BEM VINDO AO TO-DO</h1>
+        </Container>
+       
+    )
+}
+
+export default Home;
